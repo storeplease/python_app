858c5a53335813394e36f0418640df638a0adf9d
storeplease <storeplase@gmail.com>
2023-03-13 13:43:51 +0500

    test laptop win commit

---
+++ common/HelloWorld1 - Copy.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+window=tk.Tk()
+window.title("Hello World1 Test Commit_Laptop")
+window.geometry("600x400")
+newlabel = tk.Label(text = "Gimme ur money")
+newlabel.grid(column=0,row=0)
+window.mainloop()
